c5acc8a665edc1141395f92da0eee34a25f46c1f
mohamed <egyptgoschool@gmail.com>
2025-09-14 07:09:03 +0300

    Adds description field to todo items and updates frontend
    Adds a required description field to todo items in the backend schema
    Updates the frontend to use a custom icon and changes the page title
    Removes the default Vite icon from the project

--- api-todo-list-backend/types/generated/contentTypes.d.ts
+++ api-todo-list-backend/types/generated/contentTypes.d.ts
@@ -424,6 +424,7 @@ export interface ApiTodoTodo extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    description: Schema.Attribute.Text & Schema.Attribute.Required;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::todo.todo'> &
       Schema.Attribute.Private;
